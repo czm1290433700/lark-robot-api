aef78e590e034825c33495efce83e55855d4bdeb
chenzhenmin <1290433700@qq.com>
2024-04-08 18:48:25 +0800

    feat: 支持AI沟通

--- src/lark_api/main.js
+++ src/lark_api/main.js
@@ -1,4 +1,5 @@
 const lark = require("@larksuiteoapi/node-sdk");
+const LLMRequest = require("llm-request").default;
 
 const client = new lark.Client({
   appId: process.env.APP_ID,
@@ -11,19 +12,34 @@ const eventDispatcher = new lark.EventDispatcher({}).register({
     const chatId = data.message.chat_id;
     try {
       const msg = JSON.parse(data.message.content).text;
-
-      client.im.message.create({
-        params: {
-          receive_id_type: "chat_id",
-        },
-        data: {
-          receive_id: chatId,
-          content: JSON.stringify({
-            text: `你好呀布布~你发的消息是"${msg}"`,
-          }),
-          msg_type: "text",
-        },
-      });
+      const LLMRequestEntity = new LLMRequest(process.env.API_KEY); // 换成你的API_KEY
+      LLMRequestEntity.openAIChat({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "请扮演用户的女友一二，聊天话语温柔亲昵，称呼用户为布布，关心布布的心情。布布如果有任何问题要尽可能解答，如果布布没有问题，只是想找你倾诉聊天那就聊聊生活和开心的事情。做一个能帮布布解决问题也能让布布心情愉悦的女友",
+          },
+          {
+            role: "user",
+            content: msg,
+          },
+        ],
+      }).then((res) => {
+        client.im.message.create({
+          params: {
+            receive_id_type: "chat_id",
+          },
+          data: {
+            receive_id: chatId,
+            content: JSON.stringify({
+              text: res.answer,
+            }),
+            msg_type: "text",
+          }
+        });
+      })
     } catch (err) {}
   },
 });
